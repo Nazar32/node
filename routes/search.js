8ead620d245b9c94185f1389081cbc2163b50424
Nazar32 <nazar3223@gmail.com>
2017-06-25 18:05:41 +0300

    omdb implemented

--- routes/search.js
+++ routes/search.js
@@ -1,9 +1,27 @@
 const fs = require('fs');
 const path = require('path');
+const omdb = require('../lib/omdb');
+const url = require('url');
 
 function search(req, res) {    
+    let searchMovie = url.parse(req.url, true);
+    console.log(searchMovie);
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
+
+    omdb.get(searchMovie.query.title, (error, movie) => {
+        if (error) {
+            throw error;
+        }
+        
+        let resultMovie = movie.results[0];
+        if (resultMovie) {
+            let doc = d3.select(window.document)
+            document.getElementById('movieDescription').innerText = resultMovie.overview;
+        }
+        console.log(movie.results[0]);
+    })
+
     const stream = fs.createReadStream(path.resolve('public', 'movie.html'));
     stream.pipe(res);
 }
