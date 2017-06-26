8150e7f488c4fe93fc1e5cef204cb330b088e941
Nazar32 <n.melnyk@NEW-USER-VM9>
2017-06-26 17:18:50 +0300

    Case when movie not found implemented

--- routes/search.js
+++ routes/search.js
@@ -6,21 +6,35 @@ const render = require('../lib/render');
 function search(req, res) {    
     let searchMovie = url.parse(req.url, true);
 
-    omdb.get(searchMovie.query.title, (error, movie) => {
-        if (error) {
-            console.log(error);
-            throw error;
-        }
-                
-        render('movie.html', movie.results[0], (error, html) => {
-            if (error) {
-                throw error;
+    omdb.get(searchMovie.query.title, (error, data) => {
+        let movie;
+        if (data && data.results && data.results.length) {
+            movie = data.results[0];
+        } else if (!error) {
+            error = {
+                message: 'No movies found'
             }
-
+        }
+        if (error) {                      
+            return render('error.html', { error: error.message }, (error, html) => {                
+                if (error) {
+                    res.writeHead(500, {'Content-Type': 'text/plain'});
+                    return res.end(error.message);
+                }
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'text/html');
+                res.end(html);    
+            });      
+        }
+        render('movie.html', movie, (error, html) => {                
+            if (error) {
+                res.writeHead(500, {'Content-Type': 'text/plain'});
+                return res.end(error.message);
+            }
             res.statusCode = 200;
             res.setHeader('Content-Type', 'text/html');
-            res.end(html);
-        });        
+            res.end(html);    
+        });           
     })
 }
 
